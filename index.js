cbd8a96feb5e4e54a18c9765c290de74defc80cc
Kim <sambathkimchhorn@gmail.com>
2024-12-14 14:59:41 -0800

    animation uqdated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function handleClick(){
     alert("I got clicked!");
 }
 function soundLetter(letter){
+
     switch(letter) {
         case "w":
             var crash = new Audio("sounds/crash.mp3");
@@ -38,10 +39,20 @@ for(var i = 0; i < document.querySelectorAll(".drum").length; i++ ){
     document.querySelectorAll(".drum")[i].addEventListener("click" , function(){
     var buttonInnerHTML= this.innerHTML;
    soundLetter(buttonInnerHTML);
+   pressedKey(buttonInnerHTML);
 });}
 
 document.addEventListener("keydown", function(event){
   
     soundLetter(event.key);
+    pressedKey(event.key);
     
     });
+
+function pressedKey(key){
+    const active = document.querySelector("."+ key)
+    active.classList.add("pressed");
+    setTimeout(() => {
+        active.classList.remove("pressed");
+    }, 250);
+}
